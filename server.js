fb0bb40220c9bb920cdb2628d566c258b9695285
Balaji <113905374+BALAJI-BALAJI@users.noreply.github.com>
2025-10-06 13:27:02 +0530

    all done...

--- server.js
+++ server.js
@@ -33,10 +33,7 @@ app.use("/staff", staffRoutes);
 // ✅ Connect MongoDB & Start Server
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => {
   console.log("✅ MongoDB connected");
   app.listen(PORT, () =>
